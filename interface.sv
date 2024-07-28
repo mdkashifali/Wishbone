9321b2fc7c6549af802f0d4d18b8fcfc8f1285fa
mdkashifali <69759421+mdkashifali@users.noreply.github.com>
2024-07-28 20:44:28 +0530

    Update interface.sv

--- interface.sv
+++ interface.sv
@@ -1,1 +1,10 @@
-
+interface wb_if;
+    logic clk;
+    logic we;
+    logic strb;
+    logic rst;
+    logic [7:0] addr;
+    logic [7:0] wdata;
+    logic [7:0] rdata;
+    logic  ack;
+endinterface
