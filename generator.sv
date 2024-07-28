eda4da8a4f1f0c3ce9063bdb97ca1d94da6f3085
mdkashifali <69759421+mdkashifali@users.noreply.github.com>
2024-07-28 20:45:40 +0530

    Update generator.sv

--- generator.sv
+++ generator.sv
@@ -1,1 +1,31 @@
-
+class generator;
+  
+  transaction tr;
+  mailbox #(transaction) mbxgd;
+  event done; ///gen completed sending requested no. of transaction
+  event drvnext; /// dr complete its wor;
+  event sconext; ///scoreboard complete its work
+ 
+   int count = 0;
+  
+  function new( mailbox #(transaction) mbxgd);
+    this.mbxgd = mbxgd;   
+    tr =new();
+  endfunction
+  
+    task run();
+    
+      for(int i=0; i< count; i++) begin
+      assert(tr.randomize) else $error("Randomization Failed"); 
+      $display("------------------------------------------");
+      tr.display("GEN");
+      mbxgd.put(tr.copy); 
+      @(drvnext);
+      @(sconext);
+      end
+      
+    ->done;
+      
+  endtask
+  
+endclass
