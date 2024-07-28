3cde52f6a69978aa9f538157823ebad41d9194de
mdkashifali <69759421+mdkashifali@users.noreply.github.com>
2024-07-28 20:55:38 +0530

    Update tb.sv

--- tb.sv
+++ tb.sv
@@ -1,1 +1,60 @@
+module tb;
+   
+  generator gen;
+  driver drv;
+  monitor mon;
+  scoreboard sco;
+ 
+  event drvnext, sconext;
+  event done;
+  
+  mailbox #(transaction) mbxgd;
+  mailbox #(transaction) mbxms;
+  
+  wb_if vif();
+  mem_wb dut (vif.clk, vif.we, vif.strb, vif.rst, vif.addr, vif.wdata, vif.rdata, vif.ack);
+ 
+  initial begin
+    vif.clk <= 0;
+  end
+  
+  always #5 vif.clk <= ~vif.clk;
+  
+  initial begin
+ 
+    mbxgd = new();
+    mbxms = new();
+    gen = new(mbxgd);
+    drv = new(mbxgd);
+    mon = new(mbxms);
+    sco = new(mbxms);
+    gen.count = 10;
+    drv.vif = vif;
+    mon.vif = vif;
+    
+    drv.drvnext = drvnext;
+    gen.drvnext = drvnext;
+    
+    gen.sconext = sconext;
+    sco.sconext = sconext;
+    
+  end
+  
+  initial begin
+      drv.reset();
+    fork
+      gen.run();
+      drv.run();
+      mon.run();
+      sco.run();
+    join_none  
+    wait(gen.done.triggered);
+    $finish();
+  end
+   
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;   
+  end
 
+endmodule
