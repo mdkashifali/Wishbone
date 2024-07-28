8ed15b9a9dffa4be7feed7a0cc756e62a818733e
mdkashifali <69759421+mdkashifali@users.noreply.github.com>
2024-07-28 20:46:51 +0530

    Update monitor.sv

--- monitor.sv
+++ monitor.sv
@@ -1,1 +1,45 @@
-
+class monitor;
+    
+  virtual wb_if vif;
+  transaction tr;
+  
+  mailbox #(transaction) mbxms;
+ 
+  
+  function new( mailbox #(transaction) mbxms );
+    this.mbxms = mbxms;
+  endfunction
+  
+  
+  task run();
+    
+    tr = new();
+    
+    forever 
+      begin 
+        wait( vif.rst == 1'b0); 
+       repeat(5) @(posedge vif.clk);
+        @(posedge vif.clk);
+        if(vif.strb == 1'b0)
+        begin
+        tr.strb = vif.strb;
+        repeat(2) @(vif.clk);
+        $display("[MON] : STRB IS ZERO");
+        mbxms.put(tr);  
+        end
+        else
+        begin
+        @(posedge vif.ack);
+        tr.we = vif.we;
+        tr.strb = vif.strb;
+        tr.wdata = vif.wdata;
+        tr.addr = vif.addr;
+        tr.rdata = vif.rdata; 
+        @(posedge vif.clk);
+        $display("[MON] : STRB IS VALID");
+        mbxms.put(tr);  
+        end
+      end 
+  endtask
+ 
+endclass
